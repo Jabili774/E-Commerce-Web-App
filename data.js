760a1bd22b50be1b7feb09edf051a66750fb6705
Jabili774 <megna1256@gmail.com>
2026-07-04 12:32:26 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,103 @@
+// ===============================
+// ShopEase Product Data
+// ===============================
+
+const products = [
+
+{
+    id: 1,
+    name: "iPhone 15",
+    category: "Electronics",
+    price: 999,
+    image: "https://images.unsplash.com/photo-1511707171634-5f897ff02aa9?w=500"
+},
+
+{
+    id: 2,
+    name: "Samsung Galaxy S25",
+    category: "Electronics",
+    price: 899,
+    image: "https://images.unsplash.com/photo-1598327105666-5b89351aff97?w=500"
+},
+
+{
+    id: 3,
+    name: "MacBook Air M4",
+    category: "Electronics",
+    price: 1399,
+    image: "https://images.unsplash.com/photo-1517336714739-489689fd1ca8?w=500"
+},
+
+{
+    id: 4,
+    name: "Nike Air Max",
+    category: "Shoes",
+    price: 150,
+    image: "https://images.unsplash.com/photo-1542291026-7eec264c27ff?w=500"
+},
+
+{
+    id: 5,
+    name: "Adidas Sneakers",
+    category: "Shoes",
+    price: 130,
+    image: "https://images.unsplash.com/photo-1600185365483-26d7a4cc7519?w=500"
+},
+
+{
+    id: 6,
+    name: "Men's Hoodie",
+    category: "Fashion",
+    price: 45,
+    image: "https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?w=500"
+},
+
+{
+    id: 7,
+    name: "Women's T-Shirt",
+    category: "Fashion",
+    price: 30,
+    image: "https://images.unsplash.com/photo-1483985988355-763728e1935b?w=500"
+},
+
+{
+    id: 8,
+    name: "Apple Watch",
+    category: "Watches",
+    price: 399,
+    image: "https://images.unsplash.com/photo-1523275335684-37898b6baf30?w=500"
+},
+
+{
+    id: 9,
+    name: "Luxury Watch",
+    category: "Watches",
+    price: 250,
+    image: "https://images.unsplash.com/photo-1547996160-81dfa63595aa?w=500"
+},
+
+{
+    id: 10,
+    name: "Modern Sofa",
+    category: "Home",
+    price: 650,
+    image: "https://images.unsplash.com/photo-1505693416388-ac5ce068fe85?w=500"
+},
+
+{
+    id: 11,
+    name: "Wooden Chair",
+    category: "Home",
+    price: 120,
+    image: "https://images.unsplash.com/photo-1505693416388-ac5ce068fe85?w=500"
+},
+
+{
+    id: 12,
+    name: "Face Wash",
+    category: "Beauty",
+    price: 20,
+    image: "https://images.unsplash.com/photo-1556228578-8c89e6adf883?w=500"
+}
+
+];
